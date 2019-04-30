5d4f9811ddf4a3b7a4e99f1e5d2d9f3e3baee4a1
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-30 17:16:16 -0400

    Update request complete

--- Router/zooRouter.js
+++ Router/zooRouter.js
@@ -55,4 +55,20 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.put("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "Update Complete" });
+      } else {
+        res.status(404).json({ message: "Not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Update failed" });
+    });
+});
 module.exports = router;
