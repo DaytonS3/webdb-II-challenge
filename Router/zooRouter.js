c93680861c1ee0ba004864b9b29c2582258d7852
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-30 17:21:57 -0400

    MVP complete

--- Router/zooRouter.js
+++ Router/zooRouter.js
@@ -71,4 +71,21 @@ router.put("/:id", (req, res) => {
       res.status(500).json({ message: "Update failed" });
     });
 });
+
+router.delete("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "delete Complete" });
+      } else {
+        res.status(404).json({ message: "Not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "delete failed" });
+    });
+});
+
 module.exports = router;
